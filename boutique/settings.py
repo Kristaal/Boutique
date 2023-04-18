342aee9bee4aecc027cf540adc8095e8c4114b15
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-04-18 12:23:20 +0000

    deployment 1 part

--- boutique/settings.py
+++ boutique/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-md^yke^j#1hlg630(2-ry(pa=j&c(pu!)8g-k3j6+bv)%rck^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique21.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,13 +122,21 @@ WSGI_APPLICATION = 'boutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
+# DATABASES = {
+#      'default': dj_database_url.parse('postgres://wxwdvxds:QJkSYz5PoeLrE4HZHvzwf-DyB_JmnSJu@kandula.db.elephantsql.com/wxwdvxds')
+#  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
